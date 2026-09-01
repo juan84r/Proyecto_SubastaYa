a43337666905b1f5aeac3e58aadf1462de449810
Rodrigo Martín Urtasun <64498335+electr0nauta@users.noreply.github.com>
2026-08-31 21:46:22 -0300

    clases

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+	internal class User
+	{
+		public int Id { get; set; }
+		public string Email { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
+		public string PasswordHash { get; set; } = string.Empty;
+		public DateTime RegistrationDate { get; set; }
+		public Wallet? Wallet { get; set; }
+		public ICollection<Auction> PublishedAuctions { get; set; } = new List<Auction>();
+		public ICollection<Bid> Bids { get; set; } = new List<Bid>();
+		public ICollection<AudithLog> AudithLogs { get; set; } = new List<AudithLog>();
+	}
+}
---
+++ Domain/Entities/Wallet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+	internal class Wallet
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public decimal TotalBalance { get; set; }
+		public decimal LockedBalance { get; set; }
+		public decimal AvailableBalance => TotalBalance - LockedBalance;
+		public int Version { get; set; }
+		public User User { get; set; } = null!;
+		public ICollection<LedgerTransaction> Transactions { get; set; } = new List<LedgerTransaction>();
+	}
+}
