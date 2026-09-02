b98c57291f287fe2a29ed8c8c65a0cdbdc581d4a
Rodrigo Martín Urtasun <64498335+electr0nauta@users.noreply.github.com>
2026-09-02 19:40:58 -0300

    interfaces y dtos

---
+++ Application/DTOs/Users/AuthResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Users
+{
+	public class AuthResponseDto
+	{
+		public int Id { get; set; }
+		public string Email { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
+		public string Token { get; set; } = string.Empty;
+	}
+}
