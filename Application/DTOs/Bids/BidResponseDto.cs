cb3579c243f1d34198627b1b0707846783b4f163
Juan Rodrigues <84.j.rodrigues@gmail.com>
2026-09-02 20:42:33 -0300

    DTO Bids

---
+++ Application/DTOs/Bids/BidResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Bids
+{
+    public class WalletResponseDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public decimal TotalBalance { get; set; }
+        public decimal LockedBalance { get; set; }
+        public decimal AvailableBalance { get; set; }
+    }
+}
---
+++ Application/DTOs/Bids/PlaceBidRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Bids
+{
+    public class PlaceBidRequestDto
+    {
+        public int AuctionId { get; set; }
+        public decimal Amount { get; set; }
+        public int ExpectedVersion { get; set; }
+    }
+}
